d39f25db34b7c3fb4965c6afd68d515757f77553
Trey <hannamtrey@gmail.com>
2025-02-01 22:41:50 -0700

    ADD: tanner

--- tests/helpers/st.py
+++ tests/helpers/st.py
@@ -1,6 +1,20 @@
-import pytest
 from streamlit.testing.v1 import AppTest
 from pathlib import Path
+import pytest
+
+
+@pytest.fixture
+def import_page():
+    def _import_page(fname: str) -> AppTest:
+        overview_page = (
+            Path(__file__).parent.parent.parent / "src" / "app" / "st_pages" / fname
+        )
+
+        at = AppTest.from_file(str(overview_page))
+
+        return at
+
+    return _import_page
 
 
 def import_page(fname: str) -> AppTest:
